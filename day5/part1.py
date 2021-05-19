3ff8500bd95a6c57de462945e47eab9ec9aaee1c
Victor H. Leras <65517984+Skaleras@users.noreply.github.com>
2021-05-19 00:07:35 -0400

    Update part1.py
    I decided to go for lists to manage the ranges within the rows and columns

--- day5/part1.py
+++ day5/part1.py
@@ -1,8 +1,9 @@
+from math import ceil as round_up
 data = open('E:\code\AoC\day5\input.txt', 'r')
 my_input = data.read()
 seats = my_input.split('\n')
-row_range = (0, 127)
-column_range = (0, 7)
+row_range = [0, 127]
+column_range = [0, 7]
 seat_ids = list()
 for seat in seats:
     n = 1
@@ -10,36 +11,33 @@ for seat in seats:
     
     while n != 8:     # row position, the first 7 letters
         if seat_row[n-1]=='F':     #lower half
-            row_range = pass
-            n+=1
-            if n==8:
-                row = row_range[0]
-        elif seat_row[n-1]=='B':   #upper half
-            row_range = pass
+            row_range[1] = (sum(row_range))//2
             n+=1
             if n==8:
                 row = row_range[1]
+        elif seat_row[n-1]=='B':   #upper half
+            row_range[0] = round_up((sum(row_range))/2)
+            n+=1
+            if n==8:
+                row = row_range[0]
                 
     n = 1
     while n != 4:     # column position, the last 3 letters
         if seat_column[n-1]=='L':    #lower half
-            column_range = pass
+            column_range[1] = (sum(column_range))//2
             n+=1
             if n==4:
                 column = column_range[0]
         elif seat_column[n-1]=='R':   #upper half
-            column_range = pass
+            column_range[0] = round_up((sum(column_range))/2)
             n+=1
             if n==4:
                 column = column_range[1]
                 
     seat_id = row*8 + column
     seat_ids.append(seat_id)
-    row_range = (0, 127)
-    column_range (0, 7)
+    row_range = [0, 127]
+    column_range = [0, 7]
 print(max(seat_ids))
 
-# WIP
 # I could have used else instead of elif since there are only two possibles events in this case
-# I'm having trouble thinking about a proper way to select the ranges for the rows and columns
-# to update the variable.
