a642c7f482620f2d05db8170822a257e23f722b6
Victor H. Leras <65517984+Skaleras@users.noreply.github.com>
2021-05-21 23:45:29 -0400

    Debugging this  WIP

--- day6/part1.py
+++ day6/part1.py
@@ -12,10 +12,21 @@ for group in groups:
     
 counter = 0
 for answers in groups_list:
-    sums.append(counter)
-    letter_list.clear()
-    counter = 0
-    for letter in answers:
-        if letter not in letter_list:
-            letter_list.append('letter')
-            counter+=1
+    if len(answers)>1:
+        sums.append(yes_counter)
+        letter_list.clear()
+        yes_counter = 0
+        for answer in answers:
+            for letter in answer:
+                if letter not in letter_list:
+                    letter_list.append('letter')
+                    yes_counter+=1
+    else:
+        sums.append(yes_counter)
+        letter_list.clear()
+        yes_counter = 0
+        for letter in answer:
+            if letter not in letter_list:
+                letter_list.append('letter')
+                yes_counter+=1
+print(sum(sums))
