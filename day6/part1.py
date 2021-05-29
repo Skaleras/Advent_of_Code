64bc56efe8c64977d71ff2eaad1ad5473a99fbb1
Victor H. Leras <65517984+Skaleras@users.noreply.github.com>
2021-05-28 22:27:44 -0400

    Update part1.py
    I overcomplicated the data structure, here's a more simple take.

--- day6/part1.py
+++ day6/part1.py
@@ -1,32 +1,11 @@
-import re
-data = open('E:\code\AoC\day6\input.txt', 'r')
-my_input = data.read()
-groups_list = list()
-letter_list = list()
-groups = re.split('\n\n', my_input)
-sums = list()
+r = open('E:\code\AoC\day6\input.txt').read().strip('\n')
+input = r.split('\n\n')
 
-for group in groups:
-    group = group.split('\n')
-    groups_list.append(group)
-    
-counter = 0
-for answers in groups_list:
-    if len(answers)>1:
-        sums.append(yes_counter)
-        letter_list.clear()
-        yes_counter = 0
-        for answer in answers:
-            for letter in answer:
-                if letter not in letter_list:
-                    letter_list.append('letter')
-                    yes_counter+=1
-    else:
-        sums.append(yes_counter)
-        letter_list.clear()
-        yes_counter = 0
-        for letter in answer:
-            if letter not in letter_list:
-                letter_list.append('letter')
-                yes_counter+=1
-print(sum(sums))
+#Part 1:
+count = 0
+for group in input:
+    for question in 'qwertyuiopasdfghjklzxcvbnm':
+        if question in group:
+            count = count + 1
+
+print(count)
