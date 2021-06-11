f22ce79b18b08a02eee51b3e0ae6cca2f532ab4f
Victor H. Leras <65517984+Skaleras@users.noreply.github.com>
2021-06-10 23:24:53 -0400

    work in progress

--- part1.py
+++ part1.py
@@ -1,26 +1,27 @@
 with open('E:\code\AoC\day9\input.txt', 'r') as input:
     numbers = input.read().split("\n")
-#print(numbers)
+
 dict_numbers = dict()
-#print(dict_numbers)
 for index , value in enumerate(numbers):
     dict_numbers[index] = value
+    
 preamble = list()
 for index in list(range(25)):
     preamble.append(int(dict_numbers[index]))
-end = True
-index = 25
+    
+index = 24
 while True:
     index += 1
-    for j in preamble:
-        for k in preamble:
-            if dict_numbers[index] == j + k:
-                end = True
-                break
-        if end:
-            break
-    if not dict_numbers[index] == j + k:
-        break
+    last_number = dict_numbers[index]
+    last_valid_number = False
+    for inner in preamble:
+        for outer in preamble:
+            if inner + outer == last_number:
+                last_valid_number = True
+
+    if not last_valid_number:
+        last_invalid_number = last_number
+        print(last_invalid_number)
     preamble.append(dict_numbers[index])
     preamble.pop(0)
-print(dict_numbers[index])
+    #print(preamble)
