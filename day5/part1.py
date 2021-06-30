a75702752b68b62da4d20999ee320890d30544de
Victor H. Leras <65517984+Skaleras@users.noreply.github.com>
2021-06-29 22:18:31 -0400

    making it more readable

--- day5/part1.py
+++ day5/part1.py
@@ -2,20 +2,21 @@ from math import ceil as round_up
 data = open('E:\code\AoC\day5\input.txt', 'r')
 my_input = data.read()
 seats = my_input.split('\n')
-row_range = [0, 127]
-column_range = [0, 7]
+
 seat_ids = list()
 for seat in seats:
-    n = 1
+
     seat_row, seat_column = seat[:7], seat[7:]  # Separating the problem in row position and column position
-    
+    n = 1
+    row_range = [0, 127]
+    column_range = [0, 7]
     while n != 8:     # row position, the first 7 letters
         if seat_row[n-1]=='F':     #lower half
             row_range[1] = (sum(row_range))//2
             n+=1
             if n==8:
                 row = row_range[1]
-        elif seat_row[n-1]=='B':   #upper half
+        else:                       #upper half
             row_range[0] = round_up((sum(row_range))/2)
             n+=1
             if n==8:
@@ -28,7 +29,7 @@ for seat in seats:
             n+=1
             if n==4:
                 column = column_range[0]
-        elif seat_column[n-1]=='R':   #upper half
+        else:                        #upper half
             column_range[0] = round_up((sum(column_range))/2)
             n+=1
             if n==4:
@@ -36,8 +37,5 @@ for seat in seats:
                 
     seat_id = row*8 + column
     seat_ids.append(seat_id)
-    row_range = [0, 127]
-    column_range = [0, 7]
-print(max(seat_ids))
 
-# I could have used else instead of elif since there are only two possibles events in this case
+print(max(seat_ids))
