1c4ee763136831242da79b197f7f59bb0aa1691a
Victor H. Leras <65517984+Skaleras@users.noreply.github.com>
2021-07-23 23:49:05 -0400

    Rain Risk WIP 3
    It's hot, the answer is a bit short so I might have to work better around the distance calculations. Almost there.

--- part1.py
+++ part1.py
@@ -17,37 +17,57 @@
 # R stands for right
 # F stands for forward
 
-with open('E:\code\AoC\day10\input.txt', 'r') as input:
+with open('E:\code\AoC\day12\input.txt', 'r') as input:
     instructions = input.read().split('\n')
+
+def degrees_to_position(degrees, action):
+    #degrees have to be integers and action has to be string L or R
+    if degrees == 90:
+        if action == 'L':
+            return 1
+        else:
+            return -1
+    elif degrees == 180:
+            return 2
+    else:           # the last case is when it's 270 degrees
+        if action == 'L':
+            return 3
+        else:
+            return -3
 
 points = 'NWSENWSENWSE'
 facing_direction = 'E'
-x_dist = 0
-y_dist = 0
+x_dist_positive, y_dist_positive = 0, 0
+x_dist_negative, y_dist_negative = 0, 0
 for instruction in instructions:
-    if instruction[0] == 'N' or instruction[0] == 'S':
-        if instruction[0] == facing_direction:
-            y_dist+= int(instruction[1:])
-        else:
-            y_dist-= int(instruction[1:])
-            if y_dist < 0:
-                y_dist = y_dist*(-1)  
 
-    elif instruction[0] == 'W' or instruction[0] == 'E':
-        if instruction[0] == facing_direction:
-            x_dist+= int(instruction[1:])
+    boat_action = instruction[0]
+    value_action = int(instruction[1:])
+    if boat_action == 'N' or boat_action == 'S':
+        if boat_action == 'N':
+            y_dist_positive+= value_action
         else:
-            x_dist-= int(instruction[1:])
-            if x_dist < 0:
-                x_dist = x_dist*(-1)
+            y_dist_negative+= value_action
+
+    elif boat_action == 'W' or boat_action == 'E':
+        if boat_action == 'E':
+            x_dist_positive+= value_action
+        else:
+            x_dist_negative+= value_action
     
-    elif instruction[0] == 'L' or instruction[0] == 'R':
+    elif boat_action == 'L' or boat_action == 'R':
         position_index = 0
-        position_index = degrees_to_position(int(instruction[1:]), instruction[0]) + points.index(facing_direction)
+        position_index = degrees_to_position(value_action, boat_action) + points.index(facing_direction)
         facing_direction = points[position_index]
 
     else:
         if facing_direction == 'N':
-            x_dist+= int(instruction[1:])
+            y_dist_positive+= value_action
         elif facing_direction == 'S':
-            pass
+            y_dist_negative+= value_action
+        elif facing_direction == 'E':
+            x_dist_positive+= value_action
+        elif facing_direction== 'W':
+            x_dist_negative+= value_action
+
+print((x_dist_positive-x_dist_negative)+(y_dist_positive-y_dist_negative))
